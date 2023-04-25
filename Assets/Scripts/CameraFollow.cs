5c14613296a37ba9fe89836d60094a414e84d805
Lucas Leperlier <lucasleperlier1010@gmail.com>
2023-04-25 15:01:16 +0200

    adding smooth camera :heart:

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,10 +7,16 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] Transform viper;
     [SerializeField] Vector3 offset;
 
+    [SerializeField] float smoothFactor = 0.1f;
+    Vector3 smoothposition = Vector3.zero;
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = viper.position + offset;
+        // transform.position = viper.position + offset;
+        smoothposition = Vector3.Lerp(smoothposition, viper.position, smoothFactor);
+        transform.position = smoothposition + offset;
 
     }
 }
+
